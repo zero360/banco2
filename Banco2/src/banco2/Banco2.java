09f2e073b5c19c643ac5abb6e2a77f8382a33903
Alejandro <alejandrodiaznavarrete3@gmail.com>
2019-02-06 20:01:33 +0100

    Cambios

--- Banco2/src/banco2/Banco2.java
+++ Banco2/src/banco2/Banco2.java
@@ -15,7 +15,7 @@ public class Banco2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hola personicas");
+        System.out.print("Cambios Alex");
     }
     
 }
